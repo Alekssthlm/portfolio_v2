942720143a092238ee09113529bec8a94a973e50
Alekssthlm <114854430+Alekssthlm@users.noreply.github.com>
2024-04-18 11:38:39 +0200

    Edited css for stackTile component

--- src/components/StackTile.tsx
+++ src/components/StackTile.tsx
@@ -1,6 +1,6 @@
 export default function StackTile(props: { name: string; image: string }) {
   return (
-    <div className="w-[10rem] h-[10rem] p-4 rounded-lg text-white font-bold text-[1.5rem] flex flex-col gap-2 items-center hover:scale-[1.10] transition-all ease-in-out duration-300 bg-[#515151] shadow-lg shadow-[#000000]">
+    <div className="w-[10rem] h-[10rem] p-4 text-white font-bold text-[1.5rem] flex flex-col gap-2 items-center hover:scale-[1.10] transition-all ease-in-out duration-300 bg-[#515151] shadow-lg shadow-[#000000]">
       <img
         className="w-[4rem] lg:w-[6rem]"
         src={props.image}
